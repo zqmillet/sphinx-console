ae274b048a5d02dc9d795ec2576f340596d90b1d
kinopico <zqmillet@qq.com>
2022-09-03 11:08:06 +0800

    [Chore] initialize the project.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from setuptools import setup
+from setuptools import find_packages
+
+from sphinx_console import VERSION
+
+with open('sphinx_console/requirements.txt', 'r', encoding='utf8') as file:
+    install_requires = list(map(lambda x: x.strip(), file.readlines()))
+
+setup(
+    name='sphinx_console',
+    version=VERSION,
+    author='kinopico',
+    author_email='zqmillet@qq.com',
+    url='https://github.com/zqmillet/sphinx-console',
+    description='an extension for sphinx to display console in sphinx documents',
+    packages=find_packages(),
+    install_requires=install_requires,
+)
